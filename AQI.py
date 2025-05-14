c0a180ae81625a1ce12862ee08f44f09241cdb2a
Nguyễn Thế Phong <105076865+thephong1311@users.noreply.github.com>
2025-05-14 21:16:18 +0700

    Update AQI.py

--- AQI.py
+++ AQI.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 import matplotlib.dates as mdates
 
-st.title("🌫️ Nền tảng dự báo ô nhiễm không khí đa chỉ số tại làng nghề MÂY TRE ĐAN")
+st.title("🌫️ Nền tảng dự báo ô nhiễm không khí đa chỉ số tại làng nghề mây tre đan")
 st.image("0310khongkhi1.jpg", use_container_width=True)
 
 
